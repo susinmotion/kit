08a2d904bc480c3238bbb7a4d0e7711ba48221cb
Alena Gribskov <marahaya@gmail.com>
2015-06-22 15:41:15 -0400

    push users, groups and groups_users to database. not tested

--- kit.py
+++ kit.py
@@ -18,14 +18,17 @@ class User(Base):
 	__tablename__ = "users"
 
 	id = Column(Integer, primary_key=True)
-	name = Column(String(128))
-	email = Column(String(128))
+	name = Column(String(128), nullable=False)
+	email = Column(String(128), nullable=False)
+	groups_owned = relationship("Groups", backref="users.id")
 
 class Group(Base):
 	__tablename__ = "groups"
 
 	id = Column(Integer, primary_key=True)
-	name = Column(String(128))
+	name = Column(String(128), nullable=False)
+	creator_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+	users = relationship("User", secondary="groups_users_association", backref="users")
 
 groups_users=Table('groups_users_association', Base.metadata,
 	Column('group_id', Integer, ForeignKey('groups.id'))
@@ -36,12 +39,38 @@ def input_emails():
 	self_name = raw_input("Who are you? ")
 	self_email = raw_input("Your email:" )
 
+	creator = session.query(User).filter(name=self_name, email=self_email).first()
+	if creator:
+		creator_id = creator.id
+	else:
+		creator = User(name=self_name, email=self_email)
+		session.add(creator)
+		session.commit(creator)
+		creator = session.query(User).filter(name=self_name, email=self_email).first()
+		creator_id = creator.id
+
 	new_group="Y"
 	while new_group == "Y":
 		group_name = raw_input("What would you like to call your group? ")
+		group = sesson.query(Group).filter(name=group_name, creator_id=creator_id).first()
+		if group:
+			group_id = group.id
+			add_users = raw_input("You already created this group. Would you like to add more users? Please type Y or N ")
+			if add_users == "N":
+				print "Please rename your group"
+				new_group = "Y"
+				continue
+		else:
+			group = Group(name=group_name, creator_id=creator_id)
+			session.add(group)
+			session.commit()
+			group = sesson.query(Group).filter(name=group_name, creator_id=creator_id).first()
+			group_id = group.id
+
 		names = [self_name]
 		emails = [self_email]
 		new_friend = "Y"
+
 		while new_friend == "Y":
 			friend_name = raw_input("Please enter the name of a friend you'd like to keep in touch with: ")
 			friend_email = raw_input("Your friend's email: ")
@@ -51,16 +80,22 @@ def input_emails():
 			while new_friend not in ["Y", "N"]:
 				new_friend = raw_input("Please type Y or N ")
 
-		put_group_in_database(group_name, names, emails)
+		put_users_in_database_and_group(group_id, names, emails)
 
 		new_group = raw_input("Do you want to create another group? Type Y or N ").upper()
 		while new_group not in ["Y", "N"]:
 			new_group = raw_input("Please type Y or N ")
 
 
-def put_users_and_group_in_database(group_name, names, emails):
+def put_users_in_database_and_group(group_id, names, emails):
 	for i in range(len(names)):
+		new_user == session.query(User).filter(name=names[i], email=emails[i]).first()
+		if new_user:
+			new_user.group = group_id
+			continue
 		new_user = Users(names[i], emails[i])
+		session.add(new_user)
+		session.commit()
 
 
 
